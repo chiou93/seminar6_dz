cb152d88e4de8cf648253b12aeafa0d858eb0143
Leisan <dororoqw@gmail.com>
2022-09-04 05:37:33 +0300

    35 задача

--- 35/Program.cs
+++ 35/Program.cs
@@ -4,19 +4,19 @@
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
 
-int N=123;
-int[] a=new int[N];
-Random random=new Random();
-for(int i=0;i<a.Length;i++)
-a[i]=random.Next(0,10);
-for(int i=0;i<a.Length;i++)
-System.Console.Write($"{a[i],4}");
+int N = 123;
+int[] a = new int[N];
+Random random = new Random();
+for (int i = 0; i < a.Length; i++)
+    a[i] = random.Next(0, 10);
+for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],4}");
 
-int count=0;
-for(var i=0; i<a.Length;i++)
+int count = 0;
+for (var i = 0; i < a.Length; i++)
 {
-if(i>10 && i<99)
-count++;
+    if (i > 10 && i < 99)
+        count++;
 }
 System.Console.WriteLine();
-System.Console.WriteLine(count );
+System.Console.WriteLine(count);
